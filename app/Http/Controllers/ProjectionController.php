e1dadfe9fa434714ce6d375935f3d83914f8045a
Josh Forbes <jdforbes82@gmail.com>
2015-09-08 23:14:40 -0400

    initial commit

---
+++ app/Http/Controllers/ProjectionController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Goutte\Client;
+use App\Http\Controllers\Controller;
+
+class ProjectionController extends Controller
+{
+    private $mascots = [
+        'Cardinals', 'Falcons', 'Ravens',
+        'Bills', 'Panthers', 'Bears',
+        'Bengals', 'Browns', 'Cowboys',
+        'Broncos', 'Lions', 'Packers',
+        'Texans', 'Colts', 'Jaguars',
+        'Chiefs', 'Dolphins', 'Vikings',
+        'Patriots', 'Saints', 'Giants',
+        'Jets', 'Raiders', 'Eagles',
+        'Steelers', 'Chargers', '49ers',
+        'Seahawks', 'Rams', 'Buccaneers',
+        'Titans', 'Redskins'
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $client = new Client();
+
+        $teams = $this->getTeams($client);
+
+        $projections = $this->parseProjections($client);
+
+        $teams = $this->combineProjectionsWithTeams($projections, $teams);
+
+        return view('projections', [
+            'teams' => $teams
+        ]);
+    }
+
+    private function getTeams($client)
+    {
+        $teams = [];
+        $teamName = '';
+
+        $crawler = $client->request('GET', 'https://fantasybowl.com/2015/?LeagueID=28588&Page=LineupCard');
+
+        $nodes = $crawler->filter('td.tinh1, td.tinyc')->each(function ($node) {
+            return $node->text();
+        });
+
+        foreach ($nodes as $node) {
+            if (preg_match('#[0-9]#', $node)) {
+                $teamName = $node;
+            } elseif (strlen($node) < 3) {
+                continue;
+            } else {
+                $teams[$teamName][$node] = $node;
+            }
+        }
+
+        return $teams;
+    }
+
+    private function parseProjections()
+    {
+        $filename = base_path() . '/public/projections.csv';
+        $file = fopen($filename, 'r');
+
+        $players = [];
+        $header = null;
+
+        while ($row = fgetcsv($file))
+        {
+            if ($header === null)
+            {
+                $header = $row;
+                continue;
+            }
+            if (count($row) !== 16) {
+                continue;
+            }
+            $players[] = array_combine($header, $row);
+        }
+
+        $projections = [];
+
+        foreach ($players as $player) {
+            $playerName = explode(',', $player['Player']);
+
+            if ($playerName[0] == 'Jets') {
+                $playerName[1] = 'NY Jets';
+            }
+
+            if ($playerName[0] == 'Giants') {
+                $playerName[1] = 'NY Giants';
+            }
+
+            foreach ($this->mascots as $mascot) {
+                if ($mascot == $playerName[0]) {
+                    $playerName[0] = 'D';
+                }
+            }
+
+            $projections[] = [
+                'name' => ltrim(implode(' ', array_reverse($playerName))),
+                'points' => $player['Pts']
+            ];
+        }
+
+        return $projections;
+    }
+
+    private function combineProjectionsWithTeams($projections, $teams)
+    {
+        foreach ($teams as $teamName => $team) {
+            foreach ($team as $player) {
+                foreach ($projections as $projection) {
+                    if ($player == $projection['name']) {
+                        $teams[$teamName][$player] = is_numeric($projection['points']) ? $projection['points']  : 0;
+                    }
+                }
+            }
+        }
+
+        return $teams;
+    }
+
+    private function getProjections()
+    {
+        file_put_contents("projections.csv", fopen("http://www.fantasysharks.com/apps/bert/forecasts/projections.php?csv=1&Sort=&Segment=532&Position=99&scoring=2&League=-1&uid=4&uid2=&printable=", 'r'));
+    }
+}
---
+++ resources/views/projections.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand">West Coast FFL</a>
+                </div>
+            </div>
+        </nav>
+        <div class="container" style="margin-top: 20px;">
+            <div class="content">
+                <div class="jumbotron">
+                    <h1>Projected Points</h1>
+                    <p>Points pulled from Fantasy Sharks daily</p>
+                </div>
+                <div class="row">
+                    @foreach($teams as $teamName => $team)
+                        <div class="team col-md-6" style="height: 300px;">
+                            <h3>{{ $teamName }}</h3>
+                            @foreach($team as $playerName => $points)
+                                {{ $playerName }} -  {{ is_numeric($points) ? $points : 0 }} <br>
+                            @endforeach
+                            <h4>Projected Total: {{ array_sum($team) }}</h4>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
