ef2e442f0d8884c1c0a579c55f81574aa5f4b812
Josh Forbes <josh@Joshs-MacBook-Air.local>
2015-11-06 19:57:05 -0500

    Week 9

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -5,7 +5,7 @@
         <div class="content">
             <div class="jumbotron text-center" style="background-color:white;">
                 <h1>Team Points</h1>
-                <p>Week 8</p>
+                <p>Week 9</p>
             </div>
             <div class="row">
                 @foreach($teams as $teamName => $team)
